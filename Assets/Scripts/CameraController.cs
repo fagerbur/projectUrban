1892a42604a89e36f33422370fa18fd5e4205c14
Gadianton <jfagerb@gmail.com>
2020-07-11 15:05:10 -0500

    wow that limiting rotation was waaaaayyyy harder than i thought it was going to be

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //TODO: Will need to address collision in here
+    public GameObject fighter;
+    private Vector3 offsetPosition;
+    private Quaternion offsetRotation;
+
+    void Start ()
+    {
+        offsetPosition = transform.position - fighter.transform.position;
+        offsetRotation = fighter.transform.rotation;
+    }
+
+    void Update ()
+    {
+        transform.position = fighter.transform.position + offsetPosition;
+        transform.Rotate(new Vector3(offsetRotation.x, 0, offsetRotation.z));
+    }
+}
---
+++ Assets/Scripts/FighterController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FighterController : MonoBehaviour
+{
+    float mainSpeed = 100.0f;
+    private float totalRun = 1.0f;
+    public GameObject fighter;
+    Vector3 currentRotation;
+    bool maxLeftRotation = false;
+    bool maxRightRotation = false;
+
+    void Start() {
+        
+    }
+
+    void Update()
+    {
+        Vector3 velocity = GetBaseInput();
+        totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+        velocity = velocity * mainSpeed * Time.deltaTime;
+
+        Vector3 newPosition = transform.position;
+        transform.Translate(velocity);
+        newPosition.x = transform.position.x;
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+    }
+
+private Vector3 GetBaseInput()
+    {
+        Vector3 velocity = new Vector3();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if(!maxLeftRotation)
+            {
+                fighter.transform.Rotate(Vector3.down * 3);
+            }
+            transform.Rotate(new Vector3(0, -3, 0));
+
+            if((fighter.transform.eulerAngles.x * -1) < -300)
+            {
+                maxLeftRotation = true;
+                maxRightRotation = false;
+            }
+            else 
+            {
+                maxLeftRotation = false;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if(!maxRightRotation)
+            {
+                fighter.transform.Rotate(Vector3.up * 3);
+            }
+            transform.Rotate(new Vector3(0, 3, 0));
+
+            if(fighter.transform.eulerAngles.x > 300)
+            {
+                maxRightRotation = true;
+                maxLeftRotation = false;
+            }
+            else 
+            {
+                maxRightRotation = false;
+            }
+        }
+
+        return velocity;
+    }
+}
