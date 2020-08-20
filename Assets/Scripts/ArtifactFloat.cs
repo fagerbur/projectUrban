d3b3e146317cee7c45b659c290a7ead6e1ad1a63
Gadianton <jfagerb@gmail.com>
2020-08-19 21:21:39 -0500

    aww look at our cool artifact all floaty and such

---
+++ Assets/Scripts/ArtifactFloat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactFloat : MonoBehaviour
+{
+    float height = .5f;
+    float verticalSpeed = 2f;
+    float rotateSpeed = 45f;
+    Vector3 pos;
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        float newY = Mathf.Sin(Time.time * verticalSpeed) * height + pos.y;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * rotateSpeed);
+    }
+}
