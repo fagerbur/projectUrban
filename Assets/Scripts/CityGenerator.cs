9d9891bc216686b123d79a839838a91c68bb661a
Gadianton <jfagerb@gmail.com>
2020-09-16 21:13:00 -0500

    holy moly guacamole, finally got some random generation working, dang colliders being all inconsistent during awake

--- Assets/Scripts/CityGenerator.cs
+++ Assets/Scripts/CityGenerator.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class CityGenerator : MonoBehaviour
 {
-    private Object[] buildingsArray;
-    
     public RectTransform foundation;
-    // private bounds cityLimits;
+    private Object[] buildingsArray;
+    private bool isCityFull = false;
 
     void Awake()
     {
-        //This will eventually be an asset bundle
-        Debug.Log("Do we have a city?");
+        //TODO: swap resources.load to assetbundle
+        //TODO: Fix building collision so it's not sticky...
         buildingsArray = Resources.LoadAll("Buildings", typeof(GameObject));
 
-        //Need to check to make sure no buildings overlap
-        //Need to better fill the entire level
-
-        foreach (GameObject building in buildingsArray)
+        while(!isCityFull)
         {
-            Instantiate(building, new Vector3(Random.Range(-foundation.rect.width * 2, foundation.rect.width * 2), 0, Random.Range(-foundation.rect.height * 2, foundation.rect.height * 2)), Quaternion.identity);
-        }
-    }
+            int x = (int)Random.Range(0,18);
+            
+            GameObject building = (GameObject)buildingsArray[x];
+            Vector3 newLocation = new Vector3(Mathf.Round(Random.Range(-foundation.rect.width * 2, foundation.rect.width * 2) / 10) * 10f, 0, Mathf.Round(Random.Range(-foundation.rect.width * 2, foundation.rect.width * 2) / 10) * 10f);
+            Vector3 buildingBounds = building.GetComponent<MeshFilter>().sharedMesh.bounds.max;
+            Collider[] isColliding = Physics.OverlapBox(newLocation, buildingBounds);
+            int collisionCount = 0;
 
-    void Update()
-    {
-        
+            while(isColliding.Length > 1)
+            {   
+                newLocation = new Vector3(Mathf.Round(Random.Range(-foundation.rect.width * 2, foundation.rect.width * 2) / 10) * 10f, 0, Mathf.Round(Random.Range(-foundation.rect.width * 2, foundation.rect.width * 2) / 10) * 10f);
+                isColliding = Physics.OverlapBox(newLocation, buildingBounds);
+                collisionCount++;
+
+                if(collisionCount > 15)
+                {
+                    isCityFull = true;
+                    break;
+                }
+            }
+
+            if(!isCityFull)
+            {
+                Instantiate(building, newLocation, Quaternion.identity);
+            }
+        }
     }
 }
